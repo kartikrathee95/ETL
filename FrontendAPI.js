ca27ea8ad5beeec81763dd2098b3d020599e07bf
kartikrathee95 <96821019+kartikrathee95@users.noreply.github.com>
2022-06-02 13:13:07 +0530

    Update FrontendAPI.js

--- FrontendAPI.js
+++ FrontendAPI.js
@@ -1,1 +1,44 @@
 
+import axios from 'axios';
+
+import Core from 'core';
+
+export async function addICSDataGoogle(watch) {
+    
+    const url = `https://testingintegrationservice.sentieo.com/api/onenote/exportWatchlistsGoogle`
+    const BACKEND_URL = Core.integrations_env;
+    let response = {};
+    await fetch(url, {
+        method: 'post',
+        //body: JSON.stringify(watch),
+        //credentials: 'include',
+    })
+        .then(async (response) => response.json())
+        .then((res) => {
+            response = res;
+        })
+        .catch((err) => {
+            console.error(err);
+        });
+    return response;
+}
+
+export async function addICSDataOutlook(watch) {
+    
+    const url = `https://testingintegrationservice.sentieo.com/api/onenote/exportWatchlistsOutlook`
+    const BACKEND_URL = Core.integrations_env;
+    let response = {};
+    await fetch(url, {
+        method: 'post',
+        //body: JSON.stringify(watch),
+        //credentials: 'include',
+    })
+        .then(async (response) => response.json())
+        .then((res) => {
+            response = res;
+        })
+        .catch((err) => {
+            console.error(err);
+        });
+    return response;
+}
